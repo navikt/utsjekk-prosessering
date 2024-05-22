2319d2a3a43aba17b20a74a9bdef2a36d3d34e7d
Stephen Ramthun <stephenramthun@gmail.com>
2024-05-22 14:53:50 +0200

    Fjern overflødig routing

--- lib/api/routing.ts
+++
@@ -1,8 +0,0 @@
-import type { NextRequest } from 'next/server'
-
-export function getApiUrl(request: NextRequest) {
-    return request.nextUrl.href.replace(
-        request.nextUrl.origin,
-        process.env.UTSJEKK_API_HOSTNAME!
-    )
-}
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getApiUrl } from '@/lib/api/routing'
 
 const isLocal = process.env.NODE_ENV !== 'production'
 
@@ -12,10 +11,6 @@ export function middleware(request: NextRequest): NextResponse | void {
             new URL(`/oauth2/login?redirect=${url.pathname}`, request.url)
         )
     }
-
-    const apiUrl = getApiUrl(request)
-
-    return NextResponse.rewrite(apiUrl)
 }
 
 export const config = {
