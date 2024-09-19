1d1bcbb42fec720913ea32259ef77c3c171caf11
Stephen Ramthun <stephenramthun@gmail.com>
2024-09-19 18:53:14 +0200

    Fjern ubrukt kode

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,63 +1,25 @@
 import { NextRequest } from 'next/server'
 import { requireAuthHeader } from '@/lib/headers'
 
-function isTaskStatus(value: string | null): value is TaskStatus {
-    switch (value) {
-        case 'UNPROCESSED':
-        case 'PROCESSING':
-        case 'FAIL':
-        case 'COMPLETE':
-        case 'MANUAL':
-            return true
-        default:
-            return false
-    }
+type Params = {
+    kind: string // Task['kind']
+    after: datetime
+    status: string // Task['status']
 }
 
-function formatForFiltering(value: string) {
-    return isTaskStatus(value.toUpperCase())
-        ? (value.toUpperCase() as TaskStatus)
-        : null
-}
-
-function getTaskStatusFilter(request: NextRequest): (task: Task) => boolean {
-    const tasksStatuser: TaskStatus[] =
-        request.nextUrl.searchParams
-            .get('status')
-            ?.split(',')
-            .map(formatForFiltering)
-            .filter(isTaskStatus) ?? []
-    return tasksStatuser.length > 0
-        ? (task: any) => tasksStatuser.includes(task.status as TaskStatus)
-        : () => true
-}
-
-function getKindFilter(request: NextRequest): (task: Task) => boolean {
-    const kindParam = request.nextUrl.searchParams.get('kind')
-    return kindParam ? (task: Task) => task.kind === kindParam : () => true
-}
-
-function getAfterFilter(request: NextRequest) {
-    const after = request.nextUrl.searchParams.get('after')
-    return after
-        ? (task: Task) =>
-              new Date(task.createdAt).getTime() < new Date(after).getTime()
-        : () => true
-}
-
-export async function GET(request: NextRequest) {
+export async function GET(_: NextRequest, { params }: { params: Params }) {
     const authHeader = requireAuthHeader()
+    const url = new URL(`${process.env.TASK_API_BASE_URL}/api/tasks`)
 
-    return fetch(`${process.env.TASK_API_BASE_URL}/api/tasks`, {
+    if (params) {
+        Object.entries(params).forEach(([key, value]) => {
+            url.searchParams.append(key, value)
+        })
+    }
+
+    return fetch(url, {
         headers: {
             ...authHeader,
         },
     })
-
-    // const filtrerteTasks = (tasks as Task[])
-    //     .filter(getTaskStatusFilter(request))
-    //     .filter(getKindFilter(request))
-    //     .filter(getAfterFilter(request))
-
-    // return new Response(JSON.stringify(filtrerteTasks), { status: 200 })
 }
