fc7d6ef8586de1ecf430e0f4a12968b117562e82
Stephen Ramthun <stephenramthun@gmail.com>
2024-09-25 13:44:04 +0200

    Legg til mer logging

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,11 +1,14 @@
 import { NextRequest } from 'next/server'
 import { requireAuthHeader } from '@/lib/headers'
+import { logger } from '@navikt/next-logger'
 
 export async function GET(request: NextRequest) {
+    logger.info(`Route handler, henter tasks: ${request}`)
     const searchParams = request.nextUrl.searchParams
     const authHeader = requireAuthHeader()
+    logger.info(`Auth header ok ${authHeader}`)
 
-    return fetch(
+    const response = await fetch(
         `${process.env.TASK_API_BASE_URL}/api/tasks?${searchParams.toString()}`,
         {
             headers: {
@@ -13,4 +16,7 @@ export async function GET(request: NextRequest) {
             },
         }
     )
+
+    logger.info(`Response: ${response}`)
+    return response
 }
