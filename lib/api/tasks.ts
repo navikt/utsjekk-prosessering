19c06559820cfb0f9a6572312ac64d3c9b871ed8
Stephen Ramthun <stephenramthun@gmail.com>
2024-09-25 14:05:58 +0200

    ⚗️

--- lib/api/tasks.ts
+++ lib/api/tasks.ts
@@ -12,40 +12,44 @@ type FetchTasksResponse = ApiResponse<FetchTasksResponseData>
 export async function fetchTasks(
     searchParams: URLSearchParams
 ): Promise<FetchTasksResponse> {
-    if (!searchParams.get('page')) {
-        searchParams.set('page', '1')
+    return {
+        data: { tasks: [], page: 1, pageSize: 20, totalTasks: 0 },
+        error: null,
     }
-
-    logger.info(
-        `Prøver å hente tasks: ${process.env.NEXT_PUBLIC_HOSTNAME}/api/tasks?${searchParams.toString()}`
-    )
-    const response = await fetch(
-        // `${process.env.NEXT_PUBLIC_HOSTNAME}/api/tasks?${searchParams.toString()}`,
-        `http://localhost:3000/api/tasks?${searchParams.toString()}`,
-        {
-            cache: 'no-cache',
-        }
-    )
-
-    if (response.ok) {
-        logger.info('Hentet tasks')
-        const body = await response.json()
-        return {
-            data: body,
-            error: null,
-        }
-    } else {
-        logger.error(
-            'Klarte ikke hente tasks:',
-            response.status,
-            response.statusText
-        )
-        return {
-            data: null,
-            error: {
-                message: response.statusText,
-                statusCode: response.status,
-            },
-        }
-    }
+    // if (!searchParams.get('page')) {
+    //     searchParams.set('page', '1')
+    // }
+    //
+    // logger.info(
+    //     `Prøver å hente tasks: ${process.env.NEXT_PUBLIC_HOSTNAME}/api/tasks?${searchParams.toString()}`
+    // )
+    // const response = await fetch(
+    //     // `${process.env.NEXT_PUBLIC_HOSTNAME}/api/tasks?${searchParams.toString()}`,
+    //     `http://localhost:3000/api/tasks?${searchParams.toString()}`,
+    //     {
+    //         cache: 'no-cache',
+    //     }
+    // )
+    //
+    // if (response.ok) {
+    //     logger.info('Hentet tasks')
+    //     const body = await response.json()
+    //     return {
+    //         data: body,
+    //         error: null,
+    //     }
+    // } else {
+    //     logger.error(
+    //         'Klarte ikke hente tasks:',
+    //         response.status,
+    //         response.statusText
+    //     )
+    //     return {
+    //         data: null,
+    //         error: {
+    //             message: response.statusText,
+    //             statusCode: response.status,
+    //         },
+    //     }
+    // }
 }
