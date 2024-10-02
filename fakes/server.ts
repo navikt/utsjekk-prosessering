91eb6f7518e359598fb0fd92b11e05f6c284e831
Stephen Ramthun <stephenramthun@gmail.com>
2024-10-02 13:35:04 +0200

    Paginer responsen fra fake-server

--- fakes/server.ts
+++ fakes/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import { TestData } from './testData.ts'
+import QueryString from 'qs'
 
 const app = express()
 const port = 8080
+
+app.use(express.json())
 
 const tasks: Record<string, Task> = {}
 const taskHistory: Record<string, TaskHistory[]> = {}
@@ -12,17 +15,37 @@ for (const task of TestData.tasks(100)) {
     taskHistory[task.id] = TestData.taskHistory(task.id)
 }
 
+const parseStringQueryParam = (
+    value?: string | string[] | QueryString.ParsedQs | QueryString.ParsedQs[]
+): undefined | string[] => {
+    if (typeof value === 'string') {
+        return value.split(',')
+    } else if (
+        Array.isArray(value) &&
+        value.every((it) => typeof it === 'string')
+    ) {
+        return value
+    }
+
+    return undefined
+}
+
 app.get('/api/tasks', (req, res) => {
     const page = req.query.page ? +req.query.page : 1
     const pageSize = req.query.pageSize ? +req.query.pageSize : 20
+    const status = parseStringQueryParam(req.query.status) ?? []
+    const kind = typeof req.query.kind === 'string' ? req.query.kind : undefined
 
-    const start = (page - 1) * pageSize
     const allTasks = Object.values(tasks)
+        .filter((it) => status.length === 0 || status.includes(it.status))
+        .filter((it) => !kind || it.kind === kind)
+    const currentPage = Math.min(page, Math.ceil(allTasks.length / pageSize))
+    const start = (currentPage - 1) * pageSize
     const paginatedTasks = allTasks.slice(start, start + pageSize)
 
     const body = {
         tasks: paginatedTasks,
-        page: page,
+        page: currentPage,
         pageSize: pageSize,
         totalTasks: allTasks.length,
     }
@@ -34,7 +57,19 @@ app.get('/api/tasks/:taskId/history', (req, res) => {
     res.send(JSON.stringify(body)).status(200)
 })
 
-app.patch('/api/tasks/:taskId', (req, res) => {})
+app.patch('/api/tasks/:taskId', (req, res) => {
+    const requestBody = req.body as {
+        message: string
+        status: TaskStatus
+    }
+
+    tasks[req.params.taskId] = {
+        ...tasks[req.params.taskId],
+        ...requestBody,
+    }
+
+    res.sendStatus(200)
+})
 
 app.listen(port, () => {
     console.log(`Utsjekk mock listening on port ${port}`)
