b8aaf65ba428abba623a21b56479e10798a6361a
Stephen Ramthun <stephenramthun@gmail.com>
2024-11-12 12:17:51 +0100

    Send med search parameters i rekjøringen av tasks

--- components/RetryMultipleTasksButton.tsx
+++ components/RetryMultipleTasksButton.tsx
@@ -1,13 +1,18 @@
 import { Routes } from '@/lib/api/routes.ts'
 import { logger } from '@navikt/next-logger'
 import { Button } from '@navikt/ds-react'
-import { useRouter } from 'next/navigation'
+import {
+    ReadonlyURLSearchParams,
+    useRouter,
+    useSearchParams,
+} from 'next/navigation'
 
-const retryTasks = async () => {
-    const response = await fetch(Routes.internal.retryAll(), { method: 'PUT' })
+const retryTasks = async (searchParams: ReadonlyURLSearchParams) => {
+    const url = `${Routes.internal.retryAll()}?${searchParams.toString()}`
+    const response = await fetch(url, { method: 'PUT' })
 
     if (!response.ok) {
-        logger.error('Klarte ikke rekjøte task:', response)
+        logger.error('Klarte ikke rekjøre task:', response)
     }
 }
 
@@ -15,11 +20,14 @@ type Props = {
     numberOfTasks: number
 }
 
-export const RetryMultipleTasksButton: React.FC<Props> = ({ numberOfTasks }) => {
+export const RetryMultipleTasksButton: React.FC<Props> = ({
+    numberOfTasks,
+}) => {
     const router = useRouter()
+    const searchParams = useSearchParams()
 
     const onClick = async () => {
-        await retryTasks()
+        await retryTasks(searchParams)
         router.refresh()
     }
 
